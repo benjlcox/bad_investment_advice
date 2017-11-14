e89fd635ca5337a00478eca4c25e0ae0c5a11d50
Ben Cox <ben@shopify.com>
2017-11-14 13:33:22 -0500

    Remove yahoo

--- lib/stocktwits.rb
+++ lib/stocktwits.rb
@@ -18,7 +18,7 @@ class StockTwits
     "FB" => 'Facebook',"ETSY" => 'Etsy',"TWTR" => "Twitter","GPRO" => 'GoPro',"AMZN" => 'Amazon',
     "GOOG" => 'Google',"AAPL" => 'Apple',"BBRY" => 'Blackberry',"GRPN" => 'Groupon',"TSLA" => 'Tesla',
     "NFLX" => 'Netflix',"CRM" => 'Salesforce',"BABA" => 'Alibaba',"LNKD" => 'LinkedIn',
-    "MSFT" => 'Microsoft',"YHOO" => 'Yahoo'
+    "MSFT" => 'Microsoft',"AABA" => 'Altaba', "MDB" => 'MongoDB', 'MDB'
   }
 
   CURRENT_TIME = Time.now.in_time_zone('Eastern Time (US & Canada)')
